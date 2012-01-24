ebca767affb4feccb3ce12d3f6d3a4c370d1e2d6
Walter Poch <walter.poch@gmail.com>
2012-01-24 11:07:57 -0300

    Failed attemps with KnockoutJS.

--- public/js/odontograma.js
+++ public/js/odontograma.js
@@ -1,3 +1,17 @@
 jQuery(function(){
-	
+	function ViewModel(){
+		this.dientes = ko.observableArray([]);
+	};
+
+	function DienteModel(){
+		this.id = ko.observable(null);	
+		this.tratamientos = ko.observableArray([]);
+	};
+
+	vm = new ViewModel();
+	var diente = new DienteModel();
+	diente.id(18);
+	vm.dientes.push(diente);
+
+	ko.applyBindings(vm);
 });
